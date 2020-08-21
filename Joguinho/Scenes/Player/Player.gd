6f0c14b2d75cc7f82dcabd4d6a5015ad96c9ddfd
Tuiuiu <lucasdario.dariolucas@gmail.com>
2020-08-21 20:36:13 -0300

    Adiciona movimentos básicos ao personagem e um mapa teste

--- Joguinho/Scenes/Player/Player.gd
+++ Joguinho/Scenes/Player/Player.gd
@@ -1,12 +1,15 @@
 extends KinematicBody2D
 
-const GRAVITY = 200.0
-const WALK_SPEED = 500
+const GRAVITY = 4000.0
+const WALK_SPEED = 500.0
+const JUMP_FORCE = 1000.0
 
 var velocity = Vector2()
 
 func _physics_process(delta):
-    velocity.y += delta * GRAVITY
+    
+    if !is_on_floor():
+        velocity.y += delta * GRAVITY
 
     if Input.is_action_pressed("ui_left"):
         velocity.x = -WALK_SPEED
@@ -15,8 +18,19 @@ func _physics_process(delta):
     else:
         velocity.x = 0
 
+    if Input.is_action_pressed("ui_up") && is_on_floor():
+        velocity.y = -JUMP_FORCE
+        
+    if Input.is_action_just_pressed("ui_select"):
+        shoot()
+    
+
     # We don't need to multiply velocity by delta because "move_and_slide" already takes delta time into account.
 
     # The second parameter of "move_and_slide" is the normal pointing up.
     # In the case of a 2D platformer, in Godot, upward is negative y, which translates to -1 as a normal.
     move_and_slide(velocity, Vector2(0, -1))
+    
+
+func shoot():
+    pass
