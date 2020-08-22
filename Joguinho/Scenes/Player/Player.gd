ef22779598221337764d18fd07db2b4de025b42a
Tuiuiu <lucasdario.dariolucas@gmail.com>
2020-08-22 00:06:20 -0300

    PlayerCharacter virou novo player, animações adicionadas e configuradas

--- Joguinho/Scenes/Player/Player.gd
+++ Joguinho/Scenes/Player/Player.gd
@@ -10,6 +10,8 @@ var last_direction = "right"
 var baseHVelocity
 var velocity = Vector2()
 var velocityModulation = 1
+var isFlipped = false
+var isCrouched = false
 
 onready var worldNode = get_tree().get_root().get_node("World")
 
@@ -21,27 +23,52 @@ func _physics_process(delta):
     # Horizontal input
     if Input.is_action_pressed("ui_left"):
         baseHVelocity = -WALK_SPEED
+        if (!isFlipped):
+            $Sprite.flip_h = true
+        if (is_on_floor()):
+            $Sprite/AnimationPlayer.play("run")
+        isFlipped = true
         last_direction = "left"
     elif Input.is_action_pressed("ui_right"):
         baseHVelocity =  WALK_SPEED
+        if (isFlipped):
+            $Sprite.flip_h = false
+        if (is_on_floor()):
+            $Sprite/AnimationPlayer.play("run")
+        isFlipped = false
         last_direction = "right"
     else:
+        if (is_on_floor()):
+            $Sprite/AnimationPlayer.play("idle")
         baseHVelocity = 0
 
     # Jump input
-    if Input.is_action_just_pressed("ui_up") && is_on_floor():
+    if Input.is_action_just_pressed("ui_up") && is_on_floor() && !isCrouched:
+        $Sprite/AnimationPlayer.play("jump")
         velocity.y = -JUMP_FORCE
         
     # Shoot input
     if Input.is_action_just_pressed("ui_select"):
         shoot(last_direction)
     
+
     # Crouch input
-    if Input.is_action_just_pressed("ui_down"):
+    if Input.is_action_pressed("ui_down") && is_on_floor():
         velocityModulation = 0
+        isCrouched = true
+        $Sprite/AnimationPlayer.play("crouch")
+        
+    # Reset speed when leaving crouch position
     if Input.is_action_just_released("ui_down"):
         velocityModulation = 1
+        isCrouched = false
         
+    # Slide input
+    if Input.is_action_just_released("ui_slide"):
+        $Sprite/AnimationPlayer.play("slide")
+        yield(get_node("Sprite/AnimationPlayer"), "animation_finished")
+                
+
     # Incresce or reduce the horizontal velocity based on the input
     velocity.x = baseHVelocity * velocityModulation
 
