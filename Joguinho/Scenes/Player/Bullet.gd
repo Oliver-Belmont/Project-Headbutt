8b8d4844d9ca2dae0f5f904b3e6f20079924f48c
Tuiuiu <lucasdario.dariolucas@gmail.com>
2020-08-21 22:54:22 -0300

    Adiciona novo input e implementa agachamento

--- Joguinho/Scenes/Player/Bullet.gd
+++ Joguinho/Scenes/Player/Bullet.gd
@@ -5,14 +5,11 @@ const HORIZONTAL_SPEED = 1800.0
 var velocity = Vector2()
 
 func _ready():
-    # velocity.x = HORIZONTAL_SPEED
     velocity.y = 0
 
 func _physics_process(delta):
-    # We don't need to multiply velocity by delta because "move_and_slide" already takes delta time into account.
-
-    # The second parameter of "move_and_slide" is the normal pointing up.
-    # In the case of a 2D platformer, in Godot, upward is negative y, which translates to -1 as a normal.
+   
+    # Move the bullet based on its velocity
     position += velocity * delta
     
 func set_direction(direction):
--- Joguinho/Scenes/Player/Player.gd
+++ Joguinho/Scenes/Player/Player.gd
@@ -7,7 +7,9 @@ const WALK_SPEED = 500.0
 const JUMP_FORCE = 1000.0
 
 var last_direction = "right"
+var baseHVelocity
 var velocity = Vector2()
+var velocityModulation = 1
 
 onready var worldNode = get_tree().get_root().get_node("World")
 
@@ -16,40 +18,51 @@ func _physics_process(delta):
     if !is_on_floor():
         velocity.y += delta * GRAVITY
 
+    # Horizontal input
     if Input.is_action_pressed("ui_left"):
-        velocity.x = -WALK_SPEED
+        baseHVelocity = -WALK_SPEED
         last_direction = "left"
     elif Input.is_action_pressed("ui_right"):
-        velocity.x =  WALK_SPEED
+        baseHVelocity =  WALK_SPEED
         last_direction = "right"
     else:
-        velocity.x = 0
+        baseHVelocity = 0
 
+    # Jump input
     if Input.is_action_just_pressed("ui_up") && is_on_floor():
         velocity.y = -JUMP_FORCE
         
+    # Shoot input
     if Input.is_action_just_pressed("ui_select"):
         shoot(last_direction)
     
+    # Crouch input
+    if Input.is_action_just_pressed("ui_down"):
+        velocityModulation = 0
+    if Input.is_action_just_released("ui_down"):
+        velocityModulation = 1
+        
+    # Incresce or reduce the horizontal velocity based on the input
+    velocity.x = baseHVelocity * velocityModulation
 
-    # We don't need to multiply velocity by delta because "move_and_slide" already takes delta time into account.
-
-    # The second parameter of "move_and_slide" is the normal pointing up.
-    # In the case of a 2D platformer, in Godot, upward is negative y, which translates to -1 as a normal.
+    # Move the character
     move_and_slide(velocity, Vector2(0, -1))
     
 
 func shoot(direction):
     var offset
+    # Instance a new bullet
     var bullet = BulletResource.instance()
-    
+   
+    # Set bullet direction and starting position 
     bullet.set_direction(direction)
     bullet.transform = self.transform
-    
     if (direction == "left"):
         offset = Vector2(-40, 0)
     elif (direction == "right"):
         offset = Vector2(40,0)
     bullet.translate(offset)
+    
+    # Add to world
     worldNode.add_child(bullet)
     
