d5271133fe8f90beaa91dc2caac10b2184c97166
Tuiuiu <lucasdario.dariolucas@gmail.com>
2020-08-22 18:48:40 -0300

    Direção modificada nas balas e velocidade alterada

--- Joguinho/Scenes/Player/Bullet.gd
+++ Joguinho/Scenes/Player/Bullet.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-const HORIZONTAL_SPEED = 1800.0
+const HORIZONTAL_SPEED = 1000.0
 
 var velocity = Vector2()
 
@@ -11,12 +11,10 @@ func _physics_process(delta):
    
     # Move the bullet based on its velocity
     position += velocity * delta
-    
+
+# If direction == 1, its right, if direction == -1, its left    
 func set_direction(direction):
-    if (direction == "left"):
-        velocity.x = -HORIZONTAL_SPEED
-    elif (direction == "right"):
-        velocity.x = HORIZONTAL_SPEED
+    velocity.x = HORIZONTAL_SPEED * direction
 
 func _on_Bullet_body_entered(body):
     if (body.is_in_group("enemy")):
