196e07d9272bc845e97c0c0e3f6810fb0d1bb140
Tuiuiu <lucasdario.dariolucas@gmail.com>
2020-08-23 04:23:30 -0300

    Adiciona um Controlador para os inimigos da fase

---
+++ Joguinho/Scenes/NPCs/Enemies/EnemiesController.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var playerNode
+
+func _ready():
+    # Connect to player's death signal
+    #playerNode = get_parent().get_node("Character")
+    #playerNode.connect("player_death", self, "stop_enemies")
+    pass
+
+# For each enemies in the stage, call Stop when the player dies
+func stop_enemies():
+    for enemy in self.get_children():
+        enemy.stop()
+        
