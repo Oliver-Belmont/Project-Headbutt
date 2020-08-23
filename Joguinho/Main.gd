74237d7ac088d1346df3f6568449db9279590ef2
Tuiuiu <lucasdario.dariolucas@gmail.com>
2020-08-23 20:45:44 -0300

    Main e Menu prototipados e pause implementado

---
+++ Joguinho/Main.gd
@@ -0,0 +1,11 @@
+extends Node
+
+
+func _ready():
+    pass # Replace with function body.
+
+func main_game_defeat():
+    pass
+    
+func main_game_win():
+    pass
--- Joguinho/Scenes/World/WorldsScript.gd
+++ Joguinho/Scenes/World/WorldsScript.gd
@@ -3,6 +3,7 @@ extends Node2D
 onready var retroShader = $CanvasLayer/ColorRect
 onready var timerFase = $ObjectiveTimer
 onready var player = $Character
+onready var main = get_tree().get_root().get_node("Main")
 
 func _ready():
     pass
@@ -14,10 +15,10 @@ func _on_ObjectiveTimer_timeout():
     finish_game_defeat()
 
 func finish_game_defeat():
-    pass
+    main.main_game_defeat()
     
 func finish_game_win():
-    pass
+    main.main_game_win()
 
 func _on_ObjectiveArea_body_entered(body):
     final_part_reached()
