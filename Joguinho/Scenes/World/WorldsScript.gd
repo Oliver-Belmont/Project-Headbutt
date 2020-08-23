42a35e05fddfcd2e3bfc1517455cad7e29c04112
Tuiuiu <lucasdario.dariolucas@gmail.com>
2020-08-23 18:20:35 -0300

    Adicionadas funções placeholder para o script do mundo

--- Joguinho/Scenes/World/WorldsScript.gd
+++ Joguinho/Scenes/World/WorldsScript.gd
@@ -11,7 +11,16 @@ func _process(delta):
     pass
     
 func _on_ObjectiveTimer_timeout():
-    finish_game()
+    finish_game_defeat()
 
-func finish_game():
+func finish_game_defeat():
+    pass
+    
+func finish_game_win():
+    pass
+
+func _on_ObjectiveArea_body_entered(body):
+    final_part_reached()
+
+func final_part_reached():
     pass
